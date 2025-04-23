9d9b79a385fd6adbbddae2f1064b3d3bb1b646b2
Luis Valencia <luismvalenp@gmail.com>
2025-04-22 20:46:11 -0600

    Pipeline configurated

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,13 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/r4logistica-demo/",
   server: {
     host: "::",
     port: 8080,
+  },
+  build: {
+    outDir: 'docs',
   },
   plugins: [
     react(),
